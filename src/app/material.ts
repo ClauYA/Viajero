492911fe3467aba276420304b52220ff6b610cea
ClauYA <cyazoe5@gmail.com>
2019-03-04 16:15:31 -0400

    version 3

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,16 +1,22 @@
 import {MatButtonModule, MatCheckboxModule,} from '@angular/material';
 import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   imports: [
       MatButtonModule,
      MatCheckboxModule,
-     MatToolbarModule
+     MatToolbarModule,
+     MatSidenavModule,
+     MatIconModule
     ],
   exports: [
       MatButtonModule, 
       MatCheckboxModule,
-      MatToolbarModule
+      MatToolbarModule,
+      MatSidenavModule,
+      MatIconModule
     ],
 })
 export class MaterialModule { }
